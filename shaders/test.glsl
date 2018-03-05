3e7228912c4b94768f3c7492b14b1f43dd3ecbad
Andor Saga <andor.saga@gmail.com>
2018-03-05 17:26:32 -0500

    p5j cvs in page loaded.

--- shaders/test.glsl
+++ shaders/test.glsl
@@ -2,28 +2,6 @@
   precision mediump float;
 #endif
 
-uniform vec2 res;
-
-float circle(vec2 p, float r){
-  vec2 aspectRatio = vec2(res.x / res.y, 1.0);
-  vec2 uv = gl_FragCoord.xy / res * 2.0 - 1.0;
-  return 1.0 - step(r, distance(uv * aspectRatio, p));
-}
-
-float circlePx(vec2 p, float r){
-  vec2 aspectRatio = vec2(res.x / res.y, 1.0);
-  vec2 uv = (gl_FragCoord.xy / res) * 2.0 - 1.0;
-  float ra =  r/res.y * 2.0;
-  return 1.0 - step(ra, distance(uv * aspectRatio, p));
-}
-
 void main() {
-  gl_FragColor = vec4( vec3(   circlePx(vec2(0.), 100.)), 1. );
+  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
-
-/*
-    Exercises
-
-    1) Change the circle function to draw a circle using 
-    pixel radius rather than a normalized value
-*/
