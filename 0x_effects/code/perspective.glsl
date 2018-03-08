2903a54d1c5ae3d0a72b3b9dc7940ad47fc36fda
Andor Saga <andor.saga@gmail.com>
2018-03-07 22:11:40 -0500

    \.

--- 0x_effects/code/perspective.glsl
+++ 0x_effects/code/perspective.glsl
@@ -8,10 +8,11 @@ uniform vec2 u_res;
 void main() {
   vec2 uv = gl_FragCoord.xy / u_res;
   uv.y = 1.0 - uv.y;
+  
 
   // inverse division
   uv.x = mod(1.0 / uv.x, 1.0);
-  
-  gl_FragColor = texture2D(u_texture0, uv);
+  uv.x = 1.0 - uv.x;
+  gl_FragColor = vec4(.38 * vec3(gl_FragCoord.x/u_res.x), 1.0) * texture2D(u_texture0, uv);
 }
 
