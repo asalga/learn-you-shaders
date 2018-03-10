f19e030623a2cf026612bd777ec0988860d480e1
Andor Saga <andor.saga@gmail.com>
2018-03-10 14:50:51 -0500

    \.

--- src/js/init-cm.js
+++ src/js/init-cm.js
@@ -52,6 +52,7 @@ function goTo(offset) {
     // only append '0' is needed
     let dstChapter = (num < 10) ? '0' + num : num;
 
+    //asalga.github.io
     window.location.href = '/' + dstChapter;
 }
 
