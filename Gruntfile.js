d7665de0ad0bce8672a87b661e8f59e8d8db79d3
Andor Saga <andor.saga@gmail.com>
2018-03-10 13:58:52 -0500

    refac

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function(grunt) {
     const Livereload = 35729;
     const ServeStatic = require('serve-static');
 
-    // directories
     const dst = 'dst';
     const src = 'src';
     const tmp = '.tmp';
@@ -16,7 +15,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         /** 
-         * Delete all working directories and contents
          */
         clean: {
             build: {
@@ -38,6 +36,25 @@ module.exports = function(grunt) {
                 path: `http://localhost:<%= connect.options.port %>/`
             }
         },
+
+
+        /**
+         *  https://www.npmjs.com/package/grunt-processhtml
+         *  process <!-- build:include --> directives
+         */
+        // processhtml: {
+        //     dev: {
+        //         options: {
+        //             process: true,
+        //             data: config,
+        //             strip: true,
+        //         },
+        //         files: [{
+        //             src: `${src}/index.html`,
+        //             dest: `${app}/index.html`
+        //         }]
+        //     }
+        // },
 
 
         /**
@@ -70,42 +87,7 @@ module.exports = function(grunt) {
             options: {
                 spawn: true,
                 livereload: true
-            },
-            scripts_dev: {
-                files: [
-                ],
-                tasks: [
-                ],
-                options: {
-                    livereload: true
-                }
-            },
-
-            // // STYLE
-            // style: {
-            //     files: [
-            //         `src/css/style.css`
-            //     ],
-            //     tasks: [
-            //         'copy:dev'
-            //     ],
-            //     options: {
-            //         livereload: true
-            //     }
-            // },
-            // // MARKUP
-            // markup: {
-            //     files: [
-            //         `src/index.html`
-            //     ],
-            //     tasks: [
-            //         // 'copy:dev'
-            //         'processhtml'
-            //     ],
-            //     options: {
-            //         livereload: true
-            //     }
-            // }
+            }
         }
     });
 
