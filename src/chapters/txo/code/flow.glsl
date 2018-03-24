2e6cc133e893e7c469934760d351ba83185e9211
Andor Saga <andor.saga@gmail.com>
2018-03-24 10:50:15 -0400

    .

--- src/chapters/txo/code/flow.glsl
+++ src/chapters/txo/code/flow.glsl
@@ -7,10 +7,9 @@ void main(){
   
   // float v = (sin(u_time) + 2.) + u_time/5.;
   // float s = u_time * (sin(PI * mod(1.*u_time, PI/2.) / PI/2.) + 1.);
-  float phaseShift = u_time +  min(1.6,abs( sin(u_time)));
+  // float phaseShift = u_time +  min(1.6,abs( sin(u_time)));
 
-
-  float t = sin(uv.x + u_time/5.0 + phaseShift*1.0);
+  float t =  sin( uv.x + 2. * u_time + sin(PI/2.*u_time + PI)) ;
   float c = step(.5 * t, uv.y);
-  gl_FragColor = vec4(c,c,c, 1.);
+  gl_FragColor = vec4(vec3(c), 1.);
 }
--- src/chapters/txo/code/steps.glsl
+++ src/chapters/txo/code/steps.glsl
@@ -1,7 +1,8 @@
 precision mediump float;
+uniform float u_time;
 uniform vec2 u_res;
 void main(){
-	vec2 uv = gl_FragCoord.xy / u_res;
-	float c = step(0.5, mod(uv.x * 2., 1.0));
-	gl_FragColor = vec4(vec3(c), 1.);
+  vec2 p = gl_FragCoord.xy / u_res + vec2(u_time/5.);
+  float c = step(5. * p.y, floor(5. * p.x));
+  gl_FragColor = vec4(vec3(c), 1.);
 }
